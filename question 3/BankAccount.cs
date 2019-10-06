37042c79febd414947bb73b57462862341dca372
James Bishop <S00187350@mail.itsligo.ie>
2019-10-06 16:15:37 +0100

    question 3 finished

---
+++ question 3/BankAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question_3
+{
+    class BankAccount
+    {
+
+        public string AccountNumber { get; set; }
+
+        public string AccountHolder { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public BankAccount(string accountNumber, string accountHolder, decimal balance)
+        {
+            this.AccountNumber = accountNumber;
+            AccountHolder = accountHolder;
+            Balance = balance;
+        }
+
+        public void Deposit(decimal account)
+        {
+            Balance += account;
+        }
+
+        public void WithDraw(decimal amount)
+        {
+            if (Balance >= amount)
+                Balance -= amount;
+        }
+
+        public string GetAccountDetails()
+        {
+            return $"Account Number: {AccountNumber}" +
+                $"\n Account Holder: {AccountHolder}" +
+                $"\nAccount Balance: {Balance:C}";
+        }
+
+
+    }
+}
---
+++ question 3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            BankAccount acc1 = new BankAccount("123456789", "James Gallgher", 2500);
+            BankAccount acc2 = new BankAccount("987654321", "Rebbeca Jones", 4000);
+
+            Console.WriteLine(acc1.GetAccountDetails());
+            Console.WriteLine("\n");
+            Console.WriteLine(acc2.GetAccountDetails());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Adding £1000 to acoount 1");
+            acc1.Deposit(1000);
+            Console.WriteLine(acc1.GetAccountDetails());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Withdrawing £500 to acoount 1");
+            acc2.WithDraw(1000);
+            Console.WriteLine(acc2.GetAccountDetails());
+
+        }
+    }
+}
