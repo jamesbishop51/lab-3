6a61b8ca1df5e4592023d9fc627d3a024ba1ac34
James Bishop <S00187350@mail.itsligo.ie>
2019-10-06 16:36:38 +0100

    lab 3 finished

---
+++ question 6/Lunch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question_6
+{
+    class Lunch
+    {
+        public string Entree { get; set; }
+
+        public string Sides { get; set; }
+
+        public string Drink { get; set; }
+    }
+}
---
+++ question 6/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lunch lunch1, lunch2, lunch3, lunch4, lunch5;
+
+            lunch1 = new Lunch();
+            lunch2 = new Lunch();
+            lunch3 = new Lunch();
+            lunch4 = new Lunch();
+            lunch5 = new Lunch();
+
+            lunch1.Entree = "Hamburger";
+            lunch1.Sides = "Fries";
+            lunch1.Drink = "Coke";
+
+            lunch2.Entree = "Hot Dog";
+            lunch2.Sides = "Chips";
+            lunch2.Drink = "Fanta";
+
+            lunch3.Entree = "Pizza";
+            lunch3.Sides = "Salad";
+            lunch3.Drink = "Water";
+
+            lunch4.Entree = "Ham Sandwich";
+            lunch4.Sides = "Fruit";
+            lunch4.Drink = "Apple Juice";
+
+            lunch5.Entree = "Cheese Toasties";
+            lunch5.Sides = "Vegtables";
+            lunch5.Drink = "Tropical Juice";
+
+            Display(lunch1, lunch2, lunch3);
+            Console.WriteLine();
+            Display(lunch1, lunch2, lunch3, lunch4);
+            Console.WriteLine();
+            Display(lunch1, lunch2, lunch3, lunch4, lunch5);
+        }
+
+        static void Display(params Lunch[] lunches)
+        {
+            Console.WriteLine("{0,-24}{1,-12}{2,-10}",
+                "Entree", "Sides", "Drinks", "Colour", "Value");
+            foreach (Lunch lunch in lunches)
+            {
+                Console.WriteLine("{0,-24}{1,-12}{2,-10}",
+                lunch.Entree, lunch.Sides, lunch.Drink);
+            }
+
+        }
+    }
+    
+}
