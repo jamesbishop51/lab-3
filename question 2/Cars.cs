b1271a1deaf272b486dfa4482ecda3bf1688afd6
James Bishop <S00187350@mail.itsligo.ie>
2019-10-06 16:07:24 +0100

    question 2 finished

---
+++ question 2/Cars.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question_2
+{
+    class Cars
+    {
+        private string make;
+
+        public string Make
+        {
+            get
+            {
+                return this.make;
+            }
+            set
+            {
+                this.make = value;
+            }
+        }
+
+        private string model;
+
+
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+        }
+
+        private double engine;
+
+        public double Engine
+        {
+            get
+            {
+                return this.engine;
+            }
+            set
+            {
+                this.engine = value;
+            }
+        }
+
+        private int speed;
+
+        public int Speed
+        {
+            get
+            {
+                return this.speed;
+
+            }
+            set
+            {
+                this.speed = value;
+            }
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Car make: {Make}");
+            Console.WriteLine($"Car model: {Model}");
+            Console.WriteLine($"Car engine: {Engine}");
+            Console.WriteLine($"Car speed: {Speed} kph");
+            Console.WriteLine("\n");
+        }
+
+        public override string ToString()
+        {
+            return
+                string.Format($"Car make: {Make}" +
+                $"\nCar model: {Model}" +
+                $"\nCar engine: {Engine}" +
+                $"\n");
+        }
+
+        public void Accelerate()
+        {
+            Speed += 10;
+
+            Console.WriteLine($"Current speed is {Speed} kph");
+        }
+    }
+}
---
+++ question 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cars c1 = new Cars();
+            c1.Make = "Toyota";
+            c1.Model = "corolla";
+            c1.Engine = 1.7;
+            c1.Speed = 0;
+
+            Cars c2 = new Cars();
+            c2.Make = "Ford";
+            c2.Model = "Focus";
+            c2.Engine = 1.1;
+            c2.Speed = 0;
+
+            c1.DisplayInfo();
+            Console.WriteLine("\n");
+
+            Console.WriteLine(c2);
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < 10; i++)
+            {
+                c1.Accelerate();
+            }
+
+            Console.WriteLine(c1);
+        }
+    }
+}
