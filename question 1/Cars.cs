37fe6f095b84195540194ab5103c31044bafd490
James Bishop <S00187350@mail.itsligo.ie>
2019-10-06 15:49:05 +0100

    Add project files.

---
+++ question 1/Cars.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question_1
+{
+    class Cars
+    {
+        private string make;
+
+        public string Make
+        {
+            get
+            {
+                return this.make;
+            }
+            set
+            {
+                this.make = value;
+            }
+        }
+
+        private string model;
+
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+
+        }
+
+        private int speed;
+
+        public int Speed
+        {
+            get
+            {
+                return this.speed;
+            }
+            set
+            {
+                this.speed = value;
+            }
+        }
+        private double engine;
+
+        public double Engine
+        {
+            get
+            {
+                return this.engine;
+            }
+            set
+            {
+                this.engine = value;
+            }
+        }
+    }
+}
---
+++ question 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cars c1 = new Cars();
+            c1.Make = "ford";
+            c1.Model = "this one";
+            c1.Speed = 0;
+            c1.Engine = 1.6;
+
+            Cars c2 = new Cars();
+            c2.Make = "toyota";
+            c2.Model = "pick up";
+            c2.Speed = 0;
+            c2.Engine = 2.1;
+
+
+            Console.WriteLine($"make : {c1.Make} model : {c1.Model} Engine size : {c1.Engine} speed : {c1.Speed}");
+            Console.WriteLine($"make : {c2.Make} model : {c2.Model} Engine size : {c2.Engine} speed : {c2.Speed}");
+
+        }
+    }
+}
